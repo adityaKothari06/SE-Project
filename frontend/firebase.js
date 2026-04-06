6ae6ff7ae06e01c06cd18a57c530cd0b89eaf6dd
Aditya Kothari <adityakothari06@gmail.com>
2026-04-06 16:09:26 +0530

    Remove API key and use environment variables

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -2,15 +2,17 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6vf0ARa9hfu3_4eohNZLEVthJ0TmIegQ",
-  authDomain: "zero-waste-pantry-c9563.firebaseapp.com",
-  projectId: "zero-waste-pantry-c9563",
-  storageBucket: "zero-waste-pantry-c9563.firebasestorage.app",
-  messagingSenderId: "892006980312",
-  appId: "1:892006980312:web:9eb9335577d7bfde85d5ad"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
-export const auth = getAuth(app)
-auth.useDeviceLanguage()
+const app = getApps().length === 0
+  ? initializeApp(firebaseConfig)
+  : getApp();
+
+export const auth = getAuth(app);
+auth.useDeviceLanguage();
